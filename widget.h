7eddbbf595c1469f990a6f5f992ce191f0bc2da4
IvanProtsenko <ivnprotsenko@gmail.com>
2017-12-02 18:15:09 +0300

    Add files via upload

--- widget.h
+++ widget.h
@@ -2,45 +2,32 @@
 #define WIDGET_H
 
 #include <QWidget>
-#include "balls.h"
-#include "player.h"
 #include <QTimer>
-#include <QPaintEvent>
-#include <QKeyEvent>
 
 namespace Ui {
-    class Widget;
+class Widget;
 }
-
-const int NUM = 5;
 
 class Widget : public QWidget
 {
     Q_OBJECT
-    QTimer timer;
-    Ball * balls[NUM];
-    Player * player;
 
 public:
     explicit Widget(QWidget *parent = 0);
     ~Widget();
-protected:
-    void keyPressEvent(QKeyEvent * k);
-    void paintEvent(QPaintEvent * e);
-protected slots:
-    void moveAll();
-    void stop();
+
 private slots:
+    void on_pushButton_clicked();
+    void timerer();
     void on_lcdNumber_overflow();
 
-    void on_pushButton_2_clicked();
+    void on_lcdNumber_2_overflow();
 
-    void on_pushButton_clicked();
+    void on_progressBar_destroyed();
 
 private:
-    bool started;
     int lives;
-    int gamestart;
     Ui::Widget *ui;
 };
-#endif
+
+#endif // WIDGET_H
